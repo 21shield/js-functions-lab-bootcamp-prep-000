a883e79db911c0a1f1abbd9f37af8d8aa427d8ef
Netaly Ramirez <21netaly@gmail.com>
2020-03-15 04:02:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ function happyHolidays(){
 
   return `Happy holidays!`
 }
+function happyHolidaysTo(name){
+  return `Happy holidays, ${name}!`
+}
 
 function happyCustomHolidayTo(holiday,person){
 
